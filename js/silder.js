68ddea32e28c40e41aed1935acb3bf3c860110b5
mtgq <2580839896@qq.com>
2022-07-09 13:54:54 +0800

    初始化

---
+++ js/silder.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function () {
+	
+	 var obj=null;
+    var As=document.getElementById('topnav').getElementsByTagName('a');
+     obj = As[0];
+     for(i=1;i<As.length;i++){if(window.location.href.indexOf(As[i].href)>=0)
+     obj=As[i];}
+     obj.id='topnav_current';
+	 
+	  //nav
+	$("#mnavh").click(function(){
+    $("#topnav").toggle();
+	$("#mnavh").toggleClass("open");
+	});
+		
+	
+});
